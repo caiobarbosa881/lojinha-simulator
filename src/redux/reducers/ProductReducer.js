e98943034e40c525aeb547d9a64140e10d6ea4f2
Caio Barbosa <caiobarbosasoriano@gmail.com>
2022-04-19 18:02:53 -0300

    API restful

---
+++ src/redux/reducers/ProductReducer.js
@@ -0,0 +1,18 @@
+import { ActionTypes } from "../contants/action-types";
+
+const initialState = {
+    products:[{
+        id: 1,
+        title: "Camisa",
+        category: "roupas"
+    }]
+}
+
+export const productReducer = (state = initialState, {type, payload}) => {
+    switch (type) {
+        case ActionTypes.SET_PRODUCTS:
+            return state;
+        default:
+            return state;
+    }
+}
